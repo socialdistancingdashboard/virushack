cec3ca2fb241292a5d3fee6ba2b6e2e9fa5c4650
Stefanie Grewenig <sgreweni@thoughtworks.com>
2020-03-21 16:49:35 +0100

    move from old to new repo

--- airquality/airquality.py
+++ airquality/airquality.py
@@ -1,15 +1,29 @@
 import base64
+import datetime
 import os
 
 import boto3
 import requests
 
+cities = [
+    'berlin'
+]
+
 airquality_token = os.environ['AIR_QUALITY_API_TOKEN']
-url = 'https://api.waqi.info/feed/berlin/?token=' + airquality_token
 
 client = boto3.client('firehose')
 
-response = requests.get(url)
-if response.status_code == 200:
-    # loop over regions
-    response = client.put_record(DeliveryStreamName='sdd-kinesis-airquality', Record={'Data': base64.encodebytes(response.content)})
+for city in cities:
+    url = f'https://api.waqi.info/feed/{city}/?token={airquality_token}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        new_record = {
+            'landkreis_name': city,
+            'datetime': datetime.datetime.now(),
+            'lat': 123,
+            'lon': 123,
+            'airquality': response.content
+        }
+        print(new_record)
+        new_record_encoded = str(new_record).encode('utf-8')
+        response = client.put_record(DeliveryStreamName='sdd-kinesis-airquality', Record={'Data': base64.b64encode(new_record_encoded)})
