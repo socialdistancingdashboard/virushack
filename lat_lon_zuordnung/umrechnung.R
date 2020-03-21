11442bf888f971c015ea9cca8d000df73e0f45f6
quickcoffee <31934316+quickcoffee@users.noreply.github.com>
2020-03-21 14:31:28 +0100

    umrechnung mit eurostat data

--- lat_lon_zuordnung/umrechnung.R
+++ lat_lon_zuordnung/umrechnung.R
@@ -2,15 +2,28 @@ library(tidyverse)
 library(eurostat)
 library(sf)
 
-#plz zuordnung von https://www.suche-postleitzahl.org/downloads
-ort_landkreis_tbl <- read_csv("plz/zuordnung_plz_ort_landkreis.csv") %>% 
-  select(plz, ort, landkreis, bundesland)
 
-#plz shape file von https://www.suche-postleitzahl.org/downloads
-plz_sf <- read_sf("plz/plz-5stellig.shp") %>% 
-  left_join(y = ort_landkreis_tbl, by = "plz")
-
-
-
+#shape file von eurostat
 nuts_sf <- get_eurostat_geospatial(nuts_level="all", year = "2016") %>% 
-  filter(CNTR_CODE == "DE")
+  filter(CNTR_CODE == "DE" & LEVL_CODE == "3")
+
+
+#zuordnung lat/lon:
+#beispiel daten
+daten_punkte_raw <- tibble(
+  "id" = 1:3,
+  "lat" = c(11.5727, 6.9646, 9.439),
+  "long" = c(48.1368, 50.9489, 54.7843))
+
+#zu simple feature umwandeln
+daten_punkte <- daten_punkte_raw %>% 
+  st_as_sf(coords = c("lat", "long"), crs = st_crs(nuts_sf))
+
+#
+punkte_mit_kreis <- daten_punkte %>% mutate(
+  intersection_id = as.integer(st_intersects(geometry, nuts_sf)),
+  kreis = case_when(is.na(intersection_id) ~ 'keine kreis zuordnung!',
+                    !is.na(intersection_id) ~ nuts_sf$NUTS_NAME[intersection_id])
+) %>% 
+  select(-intersection_id)
+
