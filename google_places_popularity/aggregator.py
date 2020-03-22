1c1d69ef71d5a82c5d060a7be3de04296a04c415
Julius <42575200+BihJ@users.noreply.github.com>
2020-03-22 17:33:35 +0100

    update dashboard v1

--- google_places_popularity/aggregator.py
+++ google_places_popularity/aggregator.py
@@ -71,29 +71,38 @@ def to_data(landkreis, date, relative_popularity, airquality_score,hystreet_scor
     }
 
 
-
+import ast
 data["normal_popularity"] = data.apply(normal_popularity, axis = 1, result_type = "reduce")
 data["relative_popularity"] = data["current_popularity"] / data["normal_popularity"]
 data["coordinates"] = data["coordinates"].astype(str)
+lat = []
+lon = []
 for index, row in data.iterrows():
-    lat = json.loads(row["coordinates"])
-data["lat"] = data["coordinates"]
-data["lon"] =
+    lat.append(ast.literal_eval(row["coordinates"])["lat"])
+    lon.append(ast.literal_eval(row["coordinates"])["lng"])
+
+data["lat"] = lat
+data["lon"] = lon
 data["ags"] = data.apply(extract_ags_from_plz,axis = 1, result_type = "reduce" )
 data2 = data.loc[data["ags"].notna()]
 
 
-result = pd.DataFrame(data2.groupby("ags")["relative_popularity"].mean())
+result = pd.DataFrame(data2.groupby("ags")[["relative_popularity","lat", "lon"]].mean())
 
 result = result.reset_index()
 list_results = []
 for index, row in result.iterrows():
     landkreis = row['ags']
     relative_popularity = row['relative_popularity']
-    coords = row["coordinates"]
+    try:
+        lat = row["lat"]
+        lon = row["lon"]
+    except:
+        lat = None
+        lon = None#
+        continue
     data_index = json.dumps({
         'name': landkreis,
-        "coords" : coords,
         # todo time from request
         'date': str(date),
          'gmap_score' : relative_popularity
@@ -103,6 +112,8 @@ for index, row in result.iterrows():
     })
     data_index2 = {
         'name': landkreis,
+        "lon" : lon,
+        "lat" : lat,
         # todo time from request
         'date': str(date),
          'gmap_score' : relative_popularity
@@ -112,7 +123,7 @@ for index, row in result.iterrows():
     }
     list_results.append(data_index2 )
     print (data_index)
-    clientFirehose.put_record(DeliveryStreamName='sdd-kinese-aggregator',  Record={'Data':data_index })
+    # clientFirehose.put_record(DeliveryStreamName='sdd-kinese-aggregator',  Record={'Data':data_index })
 
 
     print(input)
--- virushack_dashboard2.py
+++ virushack_dashboard2.py
@@ -40,44 +40,50 @@ def load_s3_data():
 
 # county_names, county_ids, mock_scores, gmaps_scores, hystreet_scores, cycle_scores = load_mock_data()
 
+
+
 df_mock_scores = pd.DataFrame(load_s3_data())
+
 standard_orte = ["Aachen","Tübingen"]
-# df_mock_scores = pd.DataFrame({"id": county_ids, "name": county_names, "score": mock_scores,"gmap_score": gmaps_scores, "hystreet_score": hystreet_scores, "cycle_score": cycle_scores})
-places = st.sidebar.multiselect('Welche Orte?', list(df_mock_scores["name"]), standard_orte)
-date = st.sidebar.date_input('Datum', datetime.date(2011,1,1))
 
-
-#data = load_data(10)
 county_names = list(set(df_mock_scores["name"].values))
 
 response = requests.get('https://github.com/socialdistancingdashboard/virushack/raw/master/logo/logo_with_medium_text.png')
 img = Image.open(BytesIO(response.content))
 st.sidebar.image(img, use_column_width=True)
 places = st.sidebar.multiselect('Welche Orte?',list(set(county_names)), standard_orte)
-start_date = st.sidebar.date_input('Datum', datetime.date(2020,3,12))
+# start_date = st.sidebar.date_input('Datum', datetime.date(2020,3,22))
 start_date = st.sidebar.date_input('Datum', datetime.date(2020,3,22))
 
 data_sources = st.sidebar.multiselect('Welche Daten?',['gmap_score', 'hystreet_score', "cycle_score"],"gmap_score")
 
 #calculate average score based on selected data_sources
 if len(data_sources) > 0:
-    df_mock_scores["average_score"] = df_mock_scores[data_sources].mean(axis=1)
-else:
-    df_mock_scores["average_score"] = [0] * len(df_mock_scores)
+    # df_mock_scores["average_score"] = df_mock_scores[data_sources].mean(axis=1)
+    df_mock_scores["gmap_score"] = df_mock_scores[data_sources].mean(axis=1)
+# else:
+#     df_mock_scores["average_score"] = [0] * len(df_mock_scores)
 
 #filter scores based on selected places
-df_mock_scores["filtered_score"] = np.where(df_mock_scores["name"].isin(places),df_mock_scores["average_score"],[0] * len(df_mock_scores))
+df_mock_scores["filtered_score"] = np.where(df_mock_scores["name"].isin(places),df_mock_scores["gmap_score"],[0] * len(df_mock_scores))
 
 
 
 st.subheader('Social Distancing Map')
+
+
+st.altair_chart(alt.Chart(df_mock_scores[df_mock_scores["name"].isin(places)][["name","filtered_score"]]).mark_bar(size = 20).encode(
+    x='name:N',
+    y='filtered_score:Q',
+    color=alt.Color('filtered_score:Q', scale=alt.Scale(scheme='greens'))
+).properties(width=750,height=400))
 
 data_topojson_remote = alt.topo_feature(url=url_topojson, feature='counties')
 c = alt.Chart(data_topojson_remote).mark_geoshape(
     stroke='white'
 ).encode(
     color=alt.Color('filtered_score:Q', scale=alt.Scale(scheme='greens')),
-    tooltip=[alt.Tooltip("properties.name:N", title="Landkreis"),alt.Tooltip("filtered_score:N", title="Score")]
+    tooltip=[alt.Tooltip("properties.name:N", title="City"),alt.Tooltip("filtered_score:N", title="Score")]
 ).transform_lookup(
     lookup='id',
     from_=alt.LookupData(df_mock_scores, 'id', ['filtered_score'])
@@ -97,11 +103,6 @@ if len(places) > 0:
 
 st.subheader("Vergleich Soziale Distanz")
 
-st.altair_chart(alt.Chart(df_mock_scores[df_mock_scores["name"].isin(places)][["name","filtered_score"]]).mark_bar(size = 20).encode(
-    x='name:N',
-    y='filtered_score:Q',
-    color=alt.Color('filtered_score:Q', scale=alt.Scale(scheme='greens'))
-).properties(width=750,height=400))
 
 if st.checkbox("Show raw data", False):
     st.subheader("Ort gefiltert")
