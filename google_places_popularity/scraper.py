f9861040bf87d4550b7c72195a13c7e07fb861eb
Julius <42575200+BihJ@users.noreply.github.com>
2020-03-22 13:56:16 +0100

    scraper script s3 ready

--- google_places_popularity/scraper.py
+++ google_places_popularity/scraper.py
@@ -4,19 +4,25 @@ import populartimes
 from datetime import datetime
 import time
 import os
+import pandas as pd
 
-if "googleapis_keys" in os.environ:
-    api_key = os.environ["googleapis_keys"]
-else:
-    with open("api_keys.txt") as f:
-        api_key = f.readline()
 
+s3_client = boto3.client('s3')
+response = s3_client.get_object(Bucket="sdd-s3-basebucket", Key="codebuild-googleplaces/places")
+api_key = response["Body"].read().decode("utf-8")
 date = datetime.now()
-with open("place_ids/staedte_koordinaten_ueber_50k_ids.csv") as f:
-        place_ids = [id.strip() for id in f.readlines()]
+
+response = s3_client.get_object(Bucket="sdd-s3-basebucket", Key="codebuild-googleplaces/2153_staedte_koordinaten.csv")
+
+city_csv = pd.read_csv(response["Body"], sep=";", header=None)
+
+# place_ids = [id.strip() for id in city_csv.readlines()]
+
+place_ids = city_csv[0]
 
 result = []
-for x, place_id in enumerate(place_ids):
+for place_id in place_ids:
+    print("processing", place_id)
     try:
         key = api_key
         data = populartimes.get_id(key, place_id)
@@ -29,6 +35,5 @@ for x, place_id in enumerate(place_ids):
     else:
         print("No Popularity-Data for " + data["name"])
 
-client = boto3.client('s3')
-client.put_object(Body=json.dumps(result),  Bucket='sdd-s3-basebucket',
-              Key='googleplaces/{}/{}/{}/{}.json'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(date.hour).zfill(2)))
+s3_client.put_object(Body=json.dumps(result),  Bucket='sdd-s3-basebucket',
+              Key='googleplaces/{}/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(date.hour).zfill(2)))
