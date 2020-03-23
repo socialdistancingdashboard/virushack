d270974e45b543f007a0e5d2f115a3ea66cc242a
Philip <philip.kreissel@icloud.com>
2020-03-23 14:38:31 +0100

    Aggregator Code

---
+++ Aggregator/agg_gmapscore.py
@@ -0,0 +1,131 @@
+from webbrowser import get
+
+import boto3
+import json
+import time
+from datetime import date
+import pandas as pd
+import pymongo
+import csv
+
+date = date.today()
+def aggregate():
+    s3_client = boto3.client('s3')
+
+    data = pd.DataFrame()
+    clientFirehose = boto3.client('firehose')
+
+    for x in range(9,19):
+        try:
+            response = s3_client.get_object(Bucket='sdd-s3-basebucket', Key='googleplaces/{}/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(x).zfill(2)))
+            result = pd.DataFrame(json.loads(response["Body"].read()))
+            result["date"] = date
+            result["hour"] = x
+            data = data.append(result)
+        except:
+            pass
+
+    def normal_popularity(row):
+        return row["populartimes"][row["date"].weekday()]["data"][row["hour"]]
+
+
+    def getPlzDictionary ():
+        with open('zuordnung_plz_ort_landkreis.csv', newline='', encoding='utf-8') as csvfile:
+            plz_dictionary = {}
+            fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            header = next(fileReader)
+            # Check file as empty
+            if header is not None:
+                # Iterate over each row after the header in the csv
+                for row in fileReader:
+                    plz=row[3]
+                    landkreis=row[2]
+                    #print("row:"+plz+" landkreis:"+landkreis)
+                    plz_dictionary[plz]=landkreis
+
+        return plz_dictionary
+
+    def extract_ags_from_plz(row):
+        #print(row["address"])
+        plz_dictionary = getPlzDictionary()
+        plz = row["address"].split(",")[-2].split(" ")
+        for ele in plz:
+            try:
+                ags = plz_dictionary[ele]
+                #print("found ags")
+                return ags
+            except:
+                #print("error: cant find ags")
+                return plz[0]
+
+    def to_data(landkreis, date, relative_popularity, airquality_score,hystreet_score,cycle_score):
+
+        #['id', 'name', 'date', 'gmap_score', 'hystreet_score', 'cycle_score']
+         return {
+            'name': landkreis,
+            # todo time from request
+            'date': date,
+             'gmap_score' : relative_popularity
+             #"airquality_score" : airquality_score
+             #'hystreet_score' : hystreet_score
+             # 'cycle_score' : cycle_score
+        }
+
+
+    import ast
+    data["normal_popularity"] = data.apply(normal_popularity, axis = 1, result_type = "reduce")
+    data["relative_popularity"] = data["current_popularity"] / data["normal_popularity"]
+    data["coordinates"] = data["coordinates"].astype(str)
+    lat = []
+    lon = []
+    for index, row in data.iterrows():
+        lat.append(ast.literal_eval(row["coordinates"])["lat"])
+        lon.append(ast.literal_eval(row["coordinates"])["lng"])
+
+    data["lat"] = lat
+    data["lon"] = lon
+    data["ags"] = data.apply(extract_ags_from_plz,axis = 1, result_type = "reduce" )
+    data2 = data.loc[data["ags"].notna()]
+
+
+    result = pd.DataFrame(data2.groupby("ags")[["relative_popularity","lat", "lon"]].mean())
+
+    result = result.reset_index()
+    list_results = []
+    for index, row in result.iterrows():
+        landkreis = row['ags']
+        relative_popularity = row['relative_popularity']
+        try:
+            lat = row["lat"]
+            lon = row["lon"]
+        except:
+            lat = None
+            lon = None#
+            continue
+        data_index = {
+            'name': landkreis,
+            # todo time from request
+            #'date': str(date),
+            'gmap_score' : relative_popularity
+             #"airquality_score" : airquality_score
+             #'hystreet_score' : hystreet_score
+             # 'cycle_score' : cycle_score
+        }
+        data_index2 = {
+            'name': landkreis,
+            "lon" : lon,
+            "lat" : lat,
+            # todo time from request
+            'date': str(date),
+            'gmap_score' : relative_popularity
+             #"airquality_score" : airquality_score
+             #'hystreet_score' : hystreet_score
+             # 'cycle_score' : cycle_score
+        }
+        list_results.append(data_index)
+        print (data_index)
+        # clientFirehose.put_record(DeliveryStreamName='sdd-kinese-aggregator',  Record={'Data':data_index })
+
+
+        print(input)
+    return list_results
---
+++ Aggregator/aggregator.py
@@ -0,0 +1,30 @@
+import boto3
+import pandas as pd
+from datetime import date
+from agg_hystreet import aggregate as agg_hystreet
+from agg_gmapscore import aggregate as agg_gmapscore
+
+date = date.today()
+
+list_result = pd.DataFrame(columns = ['name'])
+list_result =list_result.set_index("name")
+
+
+gmapscore_list = pd.DataFrame(agg_gmapscore())
+gmapscore_list = gmapscore_list.set_index('name')
+gmapscore_list
+list_result = list_result.join(gmapscore_list, how = "outer")
+list_result
+
+hystreet_list = pd.DataFrame(agg_hystreet())
+hystreet_list = hystreet_list.set_index('name')
+list_result = list_result.join(hystreet_list, how = "outer")
+
+list_result["date"] = date
+list_result.to_csv("test.csv")
+
+dict = list_result.T.to_dict()
+
+s3_client = boto3.client('s3')
+s3_client.put_object(Bucket='sdd-s3-basebucket', Key="aggdata/live", Body=json.dumps(dict))
+s3_client.put_object(Bucket='sdd-s3-basebucket', Key='aggdata/{}/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(x).zfill(2)), Body=json.dumps(dict))
