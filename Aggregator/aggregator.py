9d6b48799358fdb34afa2690b6bd1f650bbf513c
Philip <philip.kreissel@icloud.com>
2020-03-23 14:45:04 +0100

    changes

--- Aggregator/aggregator.py
+++ Aggregator/aggregator.py
@@ -3,18 +3,16 @@ import pandas as pd
 from datetime import date
 from agg_hystreet import aggregate as agg_hystreet
 from agg_gmapscore import aggregate as agg_gmapscore
+import json
 
 date = date.today()
 
 list_result = pd.DataFrame(columns = ['name'])
 list_result =list_result.set_index("name")
 
-
 gmapscore_list = pd.DataFrame(agg_gmapscore())
 gmapscore_list = gmapscore_list.set_index('name')
-gmapscore_list
 list_result = list_result.join(gmapscore_list, how = "outer")
-list_result
 
 hystreet_list = pd.DataFrame(agg_hystreet())
 hystreet_list = hystreet_list.set_index('name')
@@ -27,4 +25,4 @@ dict = list_result.T.to_dict()
 
 s3_client = boto3.client('s3')
 s3_client.put_object(Bucket='sdd-s3-basebucket', Key="aggdata/live", Body=json.dumps(dict))
-s3_client.put_object(Bucket='sdd-s3-basebucket', Key='aggdata/{}/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(x).zfill(2)), Body=json.dumps(dict))
+s3_client.put_object(Bucket='sdd-s3-basebucket', Key='aggdata/{}/{}/{}'.format(str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), Body=json.dumps(dict)))
