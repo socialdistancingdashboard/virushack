0f541306bf9ff01a21f29ccd12c250a9bd01a55e
Julius <42575200+BihJ@users.noreply.github.com>
2020-03-24 16:50:47 +0100

    Update main.py

--- hystreet/hystreet_to_s3/main.py
+++ hystreet/hystreet_to_s3/main.py
@@ -38,5 +38,5 @@ for name, group in df.groupby('timestamp'):
     date_str = name.split('+')[0]
     date = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
     print(date.strftime("%Y-%m-%d"))
-    client.put_object(Body=group.to_json(orient='records'), Bucket='sdd-s3-basebucket2',
+    client.put_object(Body=group.to_json(orient='records'), Bucket='sdd-s3-basebucket',
                       Key='hystreet/'+str(date.year).zfill(4)+'/'+str(date.month).zfill(2)+'/'+str(date.day).zfill(2))
