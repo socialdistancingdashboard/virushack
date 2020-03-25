3168031cbaa7dabc47d7155e2ab7e55c3a607f93
Walther <thomas.walther01@sap.com>
2020-03-25 15:01:49 +0100

    added webserver update script

---
+++ streamlit-beanstalk/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum -y -q update
+sudo yum install -y python3
+sudo pip3 install pandas
+sudo pip3 install streamlit
+cd /home/ec2-user
+curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/virushack_dashboard.py
+cd /usr/bin
+sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/streamlit-beanstalk/rundashboard.sh
+cd /etc/systemd/system
+sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/streamlit-beanstalk/rundashboard.service
+sudo chmod 644 /etc/systemd/system/rundashboard.service
+sudo systemctl enable rundashboard.service
+sudo systemctl start rundashboard.service
