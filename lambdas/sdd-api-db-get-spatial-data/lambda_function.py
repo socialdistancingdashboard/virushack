ea342c22b83bea4ddd9ac855dff16886824b55f0
= <=>
2020-03-25 13:05:33 +0100

    get-spatial api done

--- lambdas/sdd-api-db-get-spatial-data/lambda_function.py
+++ lambdas/sdd-api-db-get-spatial-data/lambda_function.py
@@ -1,20 +1,32 @@
+""" This Script retrieves data from db and delivers spacial data for dashboard requests."""
+
 import json
 import pandas as pd
 import pymysql
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
+import pymysql 
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
-config = json.load(open("sdd-db.conf", "r"))
+config = json.load(open("../../credentials/credentials-aws-db.json", "r"))
 
-# Connect to the database.
-connection = pymysql.connect(
-  host=config["host"],
-  user=config["user"],
-  password=config["password"],                             
-  db=config["database"],
-  charset='utf8',
-  cursorclass=pymysql.cursors.DictCursor)
+aws_engine = create_engine(
+  ("mysql+pymysql://" +
+  config["user"] + ":" +
+  config["password"] + "@" +
+  config["host"] + ":" + 
+  str(config["port"]) + "/" +
+  config["database"]),
+  poolclass=NullPool, # dont maintain a pool of connections
+  pool_recycle=3600 # handles timeouts better, I think...
+)
 
+event = {
+  "date": "2020-01-27",
+  "categories": ["score_public_transportation_regional"],
+  "spatial_granularity": 3
+}
 
 pattern_date = re.compile(r"^\d\d\d\d-\d\d-\d\d$")
 
@@ -25,11 +37,15 @@ def on_error(e):
  }
 
 def lambda_handler(event, context):
- param_date = event["date"]
- param_categories = event["categories"]
- param_spatial_granularity = event["spatial_granularity"]
- 
  try:
+  assert "categories" in event, "Please provide cateogries as array"
+  assert "date" in event, "Provide a date as 'YYYY-MM-DD'"
+  assert "spatial_granularity" in event, "Please provide a spatial granularity identifier (1: country, 2: state, 3: district)"
+
+  param_date = event["date"]
+  param_categories = event["categories"]
+  param_spatial_granularity = event["spatial_granularity"]
+
   assert re.match(pattern_date, param_date) != None, "Please provide date as YYYY-MM-DD"
   #param_date = datetime.strptime(param_date, '%Y-%m-%d')
   assert type(param_categories) == list, "Please provide a list of categories."
@@ -39,11 +55,52 @@ def lambda_handler(event, context):
   assert param_spatial_granularity <= 3, "Please provide a spatial_granularity value of less or equal 3"
  except Exception as e:
   return on_error(e)
-  
 
- return {
- "statusCode": 200,
- "body": json.dumps({
-     "message": "hello world" + str(param_date)
- }),
-}
+
+  param_start = datetime(*[int(v) for v in param_date.split("-")])
+  param_end = param_start + timedelta(days=1)
+
+  q_categories = ",".join(param_categories)
+
+  spatial_id_lookup = {
+    1: "country_id",
+    2: "state_id",
+    3: "district_id"
+  }
+  spatial_id = spatial_id_lookup[param_spatial_granularity]
+
+  q = """
+    SELECT score_value AS score, locations.%s AS spatial_id, category
+    FROM scores
+    JOIN locations ON scores.district_id = locations.district_id
+    WHERE scores.dt >= '%s' 
+    AND scores.dt < '%s'
+    AND category IN ('%s')
+  """ % (spatial_id, param_start, param_end, q_categories)
+
+  df = pd.read_sql(q, aws_engine)
+  result = {
+    "request": {
+      "date": event["date"],
+      "categories": event["categories"],
+      "spatial_granularity": event["spatial_granularity"]
+    },
+    "data": []
+  }
+
+  for category in param_categories:
+    df_filtered = df[df.category == category]
+    result["data"].append({
+      "category": category,
+      "values": json.loads(df_filtered[["score", "spatial_id"]].loc[0:2].to_json(orient="records"))
+    })
+
+  return {
+    "statusCode": 200,
+    "body": json.dumps(result),
+  }
+
+lambda_handler(event, None)
+
+
+
