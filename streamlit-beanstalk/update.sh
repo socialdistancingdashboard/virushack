182d43dfd0f064c040b5adf1121a3a658454b79b
Thomas Walther <tho.walther@gmail.com>
2020-03-25 15:17:25 +0100

    Renamed Streamlit App

--- streamlit-beanstalk/update.sh
+++ streamlit-beanstalk/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/dashboard
+/usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/EveryoneCounts.py >/dev/null 2>&1
+cd /home/ec2-user/dashboard
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/__init__.py >/dev/null 2>&1
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/dashboard.py >/dev/null 2>&1
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/dashboard_pages.py >/dev/null 2>&1
