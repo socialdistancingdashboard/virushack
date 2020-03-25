2a0ab537ed40e7fc97516c83806359a5f5972743
Thomas Walther <tho.walther@gmail.com>
2020-03-25 21:53:22 +0100

    bugfix

--- streamlit-beanstalk/install.sh
+++ streamlit-beanstalk/install.sh
@@ -3,17 +3,16 @@ sudo yum -y -q update
 sudo yum install -y python3
 sudo pip3 install pandas
 sudo pip3 install streamlit
-cd /home/ec2-user
-curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/EveryoneCounts.py
 cd /usr/bin
 sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/streamlit-beanstalk/rundashboard.sh
 cd /etc/systemd/system
 sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/streamlit-beanstalk/rundashboard.service
 sudo chmod 644 /etc/systemd/system/rundashboard.service
 sudo systemctl enable rundashboard.service
-sudo systemctl start rundashboard.service
 cd /home/ec2-user
 mkdir dashboard
 curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/streamlit-beanstalk/update.sh
-chmod u+x update.sh
+sudo chmod u+x update.sh
+sudo ./update.sh
 echo  -e  "$(crontab -l)\n*/30 * * * * /home/ec2-user/update.sh >/dev/null 2>&1" | crontab -
+sudo systemctl start rundashboard.service
