436be560399c00516638fb53362b99b1f6d1e288
SEBSCHELLI <31341218+SEBSCHELLI@users.noreply.github.com>
2020-03-25 11:15:13 +0100

    Add files via upload

--- virushack_dashboard.py
+++ virushack_dashboard.py
@@ -44,18 +44,15 @@ def load_topojson():
     county_names = []
     county_ids = []
     for county in jsondump["objects"]["counties"]["geometries"]:
-        county_names.append(county["properties"]["name"])
+        county_names.append(county["properties"]["districtType"]+" - "+county["properties"]["name"])
         county_ids.append(county["id"])
 
     return county_names, county_ids
 
 @st.cache(persist=True)
-def load_real_data(county_names, county_ids,history_length=5):
+def load_real_data(county_names, county_ids):
 
-    min_date = datetime.datetime.now().date() - datetime.timedelta(days=history_length)
-    max_date = datetime.datetime.now().date()
-    params = {"min_date": str(min_date), "max_date": str(max_date), "data_sources":"0,1,2"}
-    response = requests.get('https://0he6m5aakd.execute-api.eu-central-1.amazonaws.com/prod',params = params)
+    response = requests.get('https://0he6m5aakd.execute-api.eu-central-1.amazonaws.com/prod')
 
     county_names_prod = []
     county_ids_prod = []
@@ -75,31 +72,27 @@ def load_real_data(county_names, county_ids,history_length=5):
             if "gmap_score" in scores:
                 gmaps_scores_prod.append(scores["gmap_score"])
             else:
-                gmaps_scores_prod.append(np.nan)
+                gmaps_scores_prod.append(None)
             if "hystreet_score" in scores:
                 hystreet_scores_prod.append(scores["hystreet_score"])
             else:
-                hystreet_scores_prod.append(np.nan)
+                hystreet_scores_prod.append(None)
             if "zug_score" in scores:
                 zug_scores_prod.append(scores["zug_score"])
             else:
-                zug_scores_prod.append(np.nan)
-
+                zug_scores_prod.append(None)
 
     df_mock_scores = pd.DataFrame(
         {"id": county_ids_prod, "name": county_names_prod, "date": dates_prod, "gmap_score": gmaps_scores_prod, "hystreet_score": hystreet_scores_prod, "zug_score": zug_scores_prod})
+
+    df_mock_scores = df_mock_scores.replace([np.inf, -np.inf], np.nan)
 
     return df_mock_scores
 
 county_names, county_ids = load_topojson()
 id_to_name = {cid:county_names[idx] for idx,cid in enumerate(county_ids)}
-df_mock_scores_full = load_real_data(county_names, county_ids)
-df_mock_scores = df_mock_scores_full.copy()
-
-# filter for yesterday's entries, so that only on day's worth of data is in df_mock_scores:
-# I guess this is relevant for the averaging
-date_yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-df_mock_scores = df_mock_scores[df_mock_scores["date"] == date_yesterday]
+df_mock_scores2 = load_real_data(county_names, county_ids)
+df_mock_scores = df_mock_scores2.copy()
 
 response = requests.get('https://github.com/socialdistancingdashboard/virushack/raw/master/logo/logo_with_medium_text.png')
 img = Image.open(BytesIO(response.content))
@@ -107,13 +100,24 @@ st.sidebar.image(img, use_column_width=True)
 st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
 st.sidebar.subheader("Datenfilter")
-available_countys = [value for value in county_names if value in df_mock_scores["name"]]
-countys = st.sidebar.multiselect('Vergleiche folgende Kreise',options = list(sorted(set(county_names))))
 data_sources_names = {'gmap_score':"Google Popularitätsdaten","hystreet_score":"Hystreet Daten", "zug_score":"Zug Daten"}
 available_data_sources = [value for value in df_mock_scores.columns if value in data_sources_names.keys()]
-data_sources = st.sidebar.selectbox('mit folgenden Daten',available_data_sources, format_func=lambda x: data_sources_names[x])
-print(data_sources)
-#start_date = st.sidebar.date_input('für den Zeitraum vom', datetime.date(2020,3,12))
+data_sources = st.sidebar.selectbox('Vergleiche folgende Daten',available_data_sources, format_func=lambda x: data_sources_names[x])
+
+#calculate average score based on selected data_sources
+if len(data_sources) == 0:
+    df_mock_scores["average_score"] = df_mock_scores[[available_data_sources]].mean(axis = 1)
+elif len(data_sources) == 1:
+    df_mock_scores["average_score"] = df_mock_scores[[data_sources]]
+else:
+    df_mock_scores["average_score"] = df_mock_scores[[data_sources]].mean(axis = 1)
+
+latest_date = pd.Series(df_mock_scores[df_mock_scores["average_score"] > 0]["date"]).values[-1]
+
+available_countys = [value for value in county_names if value in df_mock_scores[df_mock_scores["average_score"] > 0]["name"].values]
+countys = st.sidebar.multiselect('für folgende Kreise',options = available_countys)
+
+#selected_date = st.sidebar.date_input('für den Zeitraum vom', datetime.date(2020,3,24))
 #end_date = st.sidebar.date_input('bis', datetime.date(2020,3,22))
 
 st.sidebar.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
@@ -127,12 +131,6 @@ st.sidebar.markdown('''
 
 
 
-#calculate average score based on selected data_sources
-if len(data_sources) > 0:
-    df_mock_scores["average_score"] = df_mock_scores[[data_sources]].mean(axis = 1)
-else:
-    df_mock_scores["average_score"] = [0] * len(df_mock_scores)
-
 #filter scores based on selected places
 if len(countys) > 0:
     df_mock_scores["filtered_score"] = np.where(df_mock_scores["name"].isin(countys), df_mock_scores["average_score"],[0] * len(df_mock_scores))
@@ -141,12 +139,10 @@ else:
 
 df_mock_scores["date"] = pd.to_datetime(df_mock_scores["date"])
 
-germany_average = np.mean(df_mock_scores["average_score"])
-st.write("Zum Vergleich - die durchschnittliche Soziale Distanz heute in Deutschland: {0:.2f}".format(germany_average))
+germany_average = np.mean(df_mock_scores[df_mock_scores["date"] == str(latest_date)]["average_score"])
+st.write("Zum Vergleich - die durchschnittliche Soziale Distanz am {} in Deutschland: {:.2f}".format(latest_date,germany_average))
 
-
-
-st.subheader('Social Distancing Map')
+st.subheader('Social Distancing Map vom {}'.format(latest_date))
 
 url_topojson = 'https://raw.githubusercontent.com/AliceWi/TopoJSON-Germany/master/germany.json'
 data_topojson_remote = alt.topo_feature(url=url_topojson, feature='counties')
@@ -160,10 +156,13 @@ layer = alt.Chart(data_topojson_remote).mark_geoshape(
     stroke='white'
 ).encode(
     color=alt.Color('filtered_score:Q', title="Soziale Distanz", scale=alt.Scale(domain=(2, 0),scheme='redyellowgreen')),
-    tooltip=[alt.Tooltip("properties.name:N", title="Landkreis"),alt.Tooltip("filtered_score:N", title="Soziale Distanz")]
+    tooltip=[alt.Tooltip("name:N", title="Kreis"),alt.Tooltip("filtered_score:N", title="Soziale Distanz")]
 ).transform_lookup(
     lookup='id',
-    from_= alt.LookupData(df_mock_scores[df_mock_scores["filtered_score"] > 0], 'id', ['filtered_score'])
+    from_= alt.LookupData(df_mock_scores[(df_mock_scores["date"] == str(latest_date)) & (df_mock_scores["filtered_score"] > 0)], 'id', ['filtered_score'])
+).transform_lookup(
+    lookup='id',
+    from_= alt.LookupData(df_mock_scores, 'id', ['name'])
 ).properties(width=750,height = 1000)
 
 c = alt.layer(basemap, layer).configure_view(
@@ -172,38 +171,27 @@ c = alt.layer(basemap, layer).configure_view(
 st.altair_chart(c)
 
 
+df_mock_scores[df_mock_scores["name"].isin(countys)][["name", "date", "filtered_score"]].dropna()
 if len(countys) > 0:
+    st.subheader("Vergleich Soziale Distanz")
+    st.altair_chart(alt.Chart(
+        df_mock_scores[df_mock_scores["name"].isin(countys)][["name", "date", "filtered_score"]].dropna()).mark_line().encode(
+        x=alt.X('date:T', axis=alt.Axis(title='Tag', format=("%d %b"))),
+        y=alt.Y('filtered_score:Q', title="Soziale Distanz"),
+        color=alt.Color('name', title="Landkreis")
+    ).properties(
+        width=750,
+        height=400
+    ))
 
-    st.altair_chart(alt.Chart(df_mock_scores[df_mock_scores["name"].isin(countys)][["name","average_score"]]).mark_bar(size = 20).encode(
-        x = alt.X('name:N', title="Landkreis / Kreisfreie Stadt"),
-        y = alt.Y('average_score:Q', title="Soziale Distanz"),
-        color=alt.Color('average_score:Q', title="Soziale Distanz",
-                        scale=alt.Scale(domain=(2, 0), scheme='redyellowgreen')),
-    ).properties(width=750,height=400))
+response_data_sources = requests.get('https://github.com/socialdistancingdashboard/virushack/raw/master/logo/Datenquellen.PNG')
+img_data_sources = Image.open(BytesIO(response_data_sources.content))
+st.image(img_data_sources, use_column_width=True)
 
-
-#-------------
-
-if len(countys) > 0:
-    st.subheader('Zeitliche Entwicklung')
-    df_history_scores = df_mock_scores_full.copy()
-    df_history_scores["date"] = pd.to_datetime(df_history_scores["date"])
-    df_history_scores = df_history_scores[df_history_scores["name"].isin(countys)]
-
-    df_history_scores2 = pd.DataFrame()
-    for county in countys:
-        df_temp = df_history_scores[df_history_scores["name"].isin([county])][["date",data_sources]]
-        df_history_scores2["date"] = df_temp["date"].reset_index(drop=True)
-        df_history_scores2[county] = df_temp[data_sources].reset_index(drop=True)
-    df_history_table = df_history_scores2.copy()
-    df_history_scores2 = df_history_scores2.melt("date")
-    df_history_scores2 = df_history_scores2.rename(columns = {'variable':'Stadt/Landkreis'})
-
-    st.altair_chart(alt.Chart(  df_history_scores2 ).mark_line().mark_line(point=True).encode(
-        x = alt.X('date:T', title="Datum"),
-        y = alt.Y('value:Q', title=data_sources),
-        color='Stadt/Landkreis:N'
-    ).properties(width=750,height=400))
-    check_show_raw_history = st.checkbox('Rohdaten anzeigen', value=False)
-    if check_show_raw_history:
-        st.write(df_history_table)
+st.markdown("""
+    <style type='text/css'>
+        details {
+            display: none;
+        }
+    </style>
+""", unsafe_allow_html=True)
