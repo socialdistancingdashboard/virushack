af534971cd2ce6ca6474d27044f2ee0e4b192921
Thomas Walther <tho.walther@gmail.com>
2020-03-30 21:10:32 +0200

    fix

--- streamlit-beanstalk/update.sh
+++ streamlit-beanstalk/update.sh
@@ -5,5 +5,5 @@ cd /home/ec2-user/dashboard
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/__init__.py >/dev/null 2>&1
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/dashboard.py >/dev/null 2>&1
 /usr/bin/wget -N -q https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dashboard/dashboard_pages.py >/dev/null 2>&1
-cd /home/ec2-user
+cd /home/ec2-user/frontend
 git pull
