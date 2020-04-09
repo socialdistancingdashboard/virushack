f326b182bfa5ae964e9e5cb181c6aa3c92cffb6f
= <=>
2020-04-09 17:38:18 +0200

    new uploader added to cron

--- sdd-db/cronjobs/cron.sh
+++ sdd-db/cronjobs/cron.sh
@@ -3,3 +3,4 @@
 # runs all update scripts
 /usr/bin/python3 db_upload_corona.py >> cron.log
 /usr/bin/python3 db_upload_gmap_popularity.py >> cron.log
+/usr/bin/python3 db_upload_energy.py >> cron.log
