d7604f01e3bb3cc889f96ba2d784c20d165ec95c
pschultzendorff <62514638+pschultzendorff@users.noreply.github.com>
2020-06-20 13:21:23 +0000

    Update agg_parkhaeuser.py
    Fixed the code.

--- Aggregator/agg_parkhaeuser.py
+++ Aggregator/agg_parkhaeuser.py
@@ -15,18 +15,18 @@ def aggregate(date):
     s3_client = boto3.client('s3')
     response = s3_client.get_object(Bucket=settings.BUCKET, Key='parkhaeuser/{}/{}/{}/{}'.format(
         str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2), str(date.hour).zfill(2)))
-    json.loads(response["Body"].read())
-    results = []
+    results = json.loads(response["Body"].read())
+    agg_results = []
     with open('zuordnung_plz_ort_landkreis.csv', encoding='utf-8') as f:
         reader = csv.DictReader(f)
-        for city in results:
-            city_name = result['landkreis']
+        for result in results:
+            city_name = results['Landkreis']
             for row in reader:
                 if row['ort'].startswith(city_name):
                     ags = row['ags']
                     break
-            data = {'landkreis': ags,
-                'parkhaeuser_score': result['Auslastung']
-                }
-            results.append(data)
-    return results
+            agg_data = {'landkreis': ags,
+                        'parkhaeuser_score': results['Auslastung']
+                        }
+            agg_results.append(agg_data)
+    return agg_results
