90156b9af889f62275e69983bd1ea9688216789b
Thomas Walther <tho.walther@gmail.com>
2020-06-26 13:27:12 +0200

    Ubuntu

--- dash-deploy/install.sh
+++ dash-deploy/install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-sudo yum -y -q update
-sudo curl https://yum.secrethub.io/secrethub.repo --output /etc/yum/repos.d/secrethub.repo --create-dirs
-sudo yum install -y secrethub-cli
-sudo yum install -y git
-sudo yum install -y python3
-sudo yum install -y python3-devel
-sudo yum install -y gcc
-sudo pip3 install pandas dash geopandas geopy influxdb_client Flask-Caching
+sudo apt-get update -y
+sudo touch /etc/apt/sources.list.d/secrethub.list
+echo 'deb [trusted=yes] https://apt.secrethub.io stable main' | sudo tee -a /etc/apt/sources.list.d/secrethub.list
+sudo apt install -y secrethub-cli
+sudo apt install -y secrethub-cli
+sudo apt install -y git
+sudo apt install -y python3
+sudo apt install -y python3-devel
+sudo apt install -y gcc
+sudo apt install -y python3-pip
+sudo apt install -y libspatialindex-dev
+sudo pip3 install pandas dash geopandas geopy influxdb_client Flask-Caching rtree
 cd /usr/bin
 sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dash-deploy/rundashboard.sh
 cd /etc/systemd/system
 sudo curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dash-deploy/rundashboard.service
 sudo chmod 644 /etc/systemd/system/rundashboard.service
 sudo systemctl enable rundashboard.service
-cd /home/ec2-user
+cd /home/ubuntu
 git clone https://github.com/socialdistancingdashboard/frontendv2.git
 curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/master/dash-deploy/update.sh
 sudo chmod u+x update.sh
 sudo ./update.sh
-sudo chown -R ec2-user:ec2-user /home/ec2-user
+sudo chown -R ubuntu:ubuntu /home/ubuntu
 sudo systemctl start rundashboard.service
 sudo su
-crontab -u ec2-user -l
-echo -e "*/30 * * * * /home/ec2-user/update.sh >/dev/null 2>&1" | crontab -u ec2-user -
+crontab -u ubuntu -l
+echo -e "*/30 * * * * /home/ubuntu/update.sh >/dev/null 2>&1" | crontab -u ubuntu -
 exit
