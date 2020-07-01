75b2b7f02b03cd71b5bd3e96e04feeef1bcc8d4c
Thomas Walther <tho.walther@gmail.com>
2020-07-01 18:24:30 +0200

    config secrethub

--- dash-deploy/install.sh
+++ dash-deploy/install.sh
@@ -21,7 +21,7 @@ curl -O https://raw.githubusercontent.com/socialdistancingdashboard/virushack/ma
 sudo chmod u+x update.sh
 sudo ./update.sh
 sudo chown -R ubuntu:ubuntu /home/ubuntu
-secrethub inject -i credentials.txt.tpl -o credentials.txt.test --identity-provider=aws
+secrethub inject -i config.json.tpl -o config.json --identity-provider=aws
 sudo systemctl start rundashboard.service
 sudo su
 crontab -u ubuntu -l
