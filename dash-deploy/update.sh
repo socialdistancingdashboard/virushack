a2e07d56a0025bfbb6fc2f9ca435233bd3d1fb36
Thomas Walther <tho.walther@gmail.com>
2020-08-03 23:25:39 +0200

    Update update.sh

--- dash-deploy/update.sh
+++ dash-deploy/update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/frontendv2
 git fetch --all && git checkout --force "origin/master"
+secrethub inject -i config.json.tpl -o config.json --identity-provider=aws --force
+sudo systemctl restart rundashboard.service
